867291ed577a8b6e1f8400ffb00aa38cd52d7b12
Zahorán Bence <h676093@stud.u-szeged.hu>
2020-01-29 23:41:19 +0100

    update index.js

--- server/index.js
+++ server/index.js
@@ -10,6 +10,11 @@ const app = express();
 app.use(bodyParser.json())
 app.use(cors())
 
+app.use(require('morgan')('combined'));
+app.use(session({ secret: 'super secret' })); //to make passport remember the user on other pages too.(Read about session store. I used express-sessions.)
+app.use(passport.initialize());
+app.use(passport.session());
+
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -27,47 +32,54 @@ app.post('/register', function (req, res) {
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log(req.body.name+" sikeresen regisztrált");
-    res.post('registration', { ok: true});
+    res.send('registration', { ok: true});
   });
 });
 
 passport.serializeUser(function(user, done) { //In serialize user you decide what to store in the session. Here I'm storing the user id only.
-  done(null, user.id, user.name);
+  done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) { //Here you retrieve all the info of the user from the session storage using the user id stored in the session earlier using serialize user.
   con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM users", function (err, result, fields) {
+    con.query("SELECT * FROM users WHERE id = "+id+" ", function (err, result, fields) {
       if (err) throw err;
       console.log(result);
-      done(err, result);
+      done(err, result[0].id);
     });
   });
 });
 
 passport.use(new LocalStrategy(function(username, password, done) {
-    /*db.findOne({'username':username},function(err,student){
-        if(err)return done(err,{message:message});//wrong roll_number or password; 
-        var pass_retrieved = student.pass_word;
-        bcrypt.compare(password, pass_retrieved, function(err3, correct) {
+  con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM users WHERE name = "+username+" ", function (err, result, fields) {
+      if(err) return done(err,{message:message});//wrong roll_number or password; 
+        var pass_retrieved = password;
+        bcrypt.compare(result[0].password, pass_retrieved, function(err3, correct) {
           if(err3){
             message = [{"msg": "Incorrect Password!"}];
             return done(null,false,{message:message});  // wrong password
           }       
           if(correct){
-              return done(null,student);
+              return done(null,user);
           } 
         });
-    });*/
+    });
+  });
 }));
 
-app.use(session({ secret: 'super secret' })); //to make passport remember the user on other pages too.(Read about session store. I used express-sessions.)
-app.use(passport.initialize());
-app.use(passport.session());
+
 
 app.post('/login',passport.authenticate('local',{successRedirect:'/home', failureRedirect: '/registration'}),
     function(req,res,next){
 });
+
+app.get('/home',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  });
  
 app.listen(3000);
