779a9703c73baf55435b7df1a4e4061b880840ed
Irinyi Kabinet Magyar Nyelvű Felhasználó <valaki@pc22508-3.inf.u-szeged.hu>
2020-02-03 15:39:18 +0100

    update home design

--- client/src/App.vue
+++ client/src/App.vue
@@ -17,6 +17,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
+}
+body,html{
+  height: 100%;
 }
 </style>
