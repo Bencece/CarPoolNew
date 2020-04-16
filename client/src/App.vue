0f47388a94b85ae543a2f846a5c59330f5eda3ea
Bencece <zahoranbence@hotmail.com>
2020-04-16 20:35:12 +0200

    delete none necessary menu

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,18 +2,6 @@
   <div id="app">
     <div id="nav">
       <router-link v-if="loggedIn" to="/dashboard">Dashboard</router-link> 
-      <div v-if="!loggedIn">
-        <router-link to="/">
-          Login
-        </router-link> |
-        <router-link to="/register">
-          Register
-        </router-link>
-      </div>
-      <button v-else type="button" class="logoutButton" @click="logout">
-        Logout
-      </button>
-    </div>
     <router-view/>
   </div>
 </template>
