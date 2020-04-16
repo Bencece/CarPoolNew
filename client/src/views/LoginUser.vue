81d9593e745086d3e68aa687f15acebbd851e6df
Bencece <zahoranbence@hotmail.com>
2020-04-16 20:34:12 +0200

    style LoginUser

--- client/src/views/LoginUser.vue
+++ client/src/views/LoginUser.vue
@@ -1,25 +1,21 @@
 <template>
   <div>
-    <form @submit.prevent="login">
-      <div class="form-group">
-        <label for="email">
-          Email:
-        </label>
-        <input v-model="email" type="email" name="email" value class="form-control">
-      </div>
-      <div class="form-group">
-        <label for="password">
-          Password:
-        </label>
-        <input v-model="password" type="password" name="password" value class="form-control">
-      </div>
-      <button type="submit" name="button">
-        Login
-      </button>
-      <router-link to="/register">
-        Don't have an account? Register.
-      </router-link>
-    </form>
+    <div class="jumbotron loginBox">
+      <h1 class="title">Üdvözlünk!</h1>
+      <form @submit.prevent="login">
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input v-model="email" type="email" name="email" value class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="password">Jelszó:</label>
+          <input v-model="password" type="password" name="password" value class="form-control">
+        </div>
+        <button type="submit" name="button" class="btn btn-success loginButton">Bejelentkezés</button>
+        <br/>
+        <router-link to="/register" class="regLabel">Nincs fiókod? Regisztrálj itt...</router-link>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -47,4 +43,19 @@ export default {
 </script>
 
 <style>
+.loginBox{
+  width: 50%;
+  margin: auto;
+  position: relative;
+  top:50%;
+}
+.title{
+  text-align: center;
+}
+.loginButton{
+  float: left;
+}
+.regLabel{
+  float: right;
+}
 </style>
