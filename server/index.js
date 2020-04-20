dc3622a39f5f8110f2b6792179bd83905b8c2226
Bencece <zahoranbence@hotmail.com>
2020-04-20 17:31:01 +0200

    user login works with database

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
-const express = require('express')
-const jwt = require('jsonwebtoken')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const fs = require('fs')
-const events = require('./db/events.json')
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const events = require('./db/events.json');
+const mysql = require('mysql');
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "carpool"
+});
 
 app.get('/', (req, res) => {
   res.json({
@@ -36,47 +44,57 @@ app.post('/register', (req, res) => {
       password: req.body.password
       // In a production app, you'll want to encrypt the password
     }
-
-    const data = JSON.stringify(user, null, 2)
-    var dbUserEmail = require('./db/user.json').email
-
-    if (dbUserEmail === req.body.email) {
-      res.sendStatus(400)
-    } else {
-      fs.writeFile('./db/user.json', data, err => {
-        if (err) {
-          console.log(err + data)
-        } else {
-          const token = jwt.sign({ user }, 'the_secret_key')
-          // In a production app, you'll want the secret key to be an environment variable
-          res.json({
-            token,
-            email: user.email,
-            name: user.name
-          })
-        }
-      })
-    }
+    console.log(user);
+    con.query("SELECT * FROM users WHERE email='"+user.email+"'", function(err, userdb){
+      if (err){
+        console.log(err);
+      } else if (userdb[0] == []){
+        res.sendStatus(400); //Ha van már ilyen felhasználó
+      } else {
+        con.query("INSERT INTO users (username, email, password) VALUES('"+user.name+"', '"+user.email+"', '"+user.password+"')", function(err, result){
+          if (err){
+            console.log(err + result);
+          } else {
+            const token = jwt.sign({ user }, 'the_secret_key')
+            // In a production app, you'll want the secret key to be an environment variable
+            res.json({
+              token,
+              email: user.email,
+              name: user.name
+            })
+          }
+        });
+      }
+    });
   } else {
     res.sendStatus(400)
   }
 })
 
 app.post('/login', (req, res) => {
-  const userDB = fs.readFileSync('./db/user.json')
-  const userInfo = JSON.parse(userDB)
-  if (
-    req.body &&
-    req.body.email === userInfo.email &&
-    req.body.password === userInfo.password
-  ) {
-    const token = jwt.sign({ userInfo }, 'the_secret_key')
-    // In a production app, you'll want the secret key to be an environment variable
-    res.json({
-      token,
-      email: userInfo.email,
-      name: userInfo.name
-    })
+  if (req.body) {
+    const user = {
+      email: req.body.email,
+      password: req.body.password
+    }
+    con.query("SELECT * FROM users WHERE email='"+user.email+"'", function(err, userdb){
+      if (err){
+        console.log(err);
+        res.sendStatus(400);
+      } else if (user.email === userdb[0].email && user.password === userdb[0].password){
+        const userInfo = {
+          email: userdb[0].email,
+          name: userdb[0].name
+        }
+        const token = jwt.sign({ userInfo }, 'the_secret_key')
+        // In a production app, you'll want the secret key to be an environment variable
+        res.json({
+          token,
+          email: userInfo.email,
+          name: userInfo.name
+        })
+      }
+    });
   } else {
     res.sendStatus(400)
   }
