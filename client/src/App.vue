e24ddfb694f259c53dae477858e25ca69504c6b6
Bencece <zahoranbence@hotmail.com>
2020-09-07 16:41:26 +0200

    dashboard changes

--- client/src/App.vue
+++ client/src/App.vue
@@ -57,6 +57,7 @@ export default {
   /*box-shadow: 0px 0px 5px 1px black;*/
   min-height: 90vh;
   box-shadow: 0 16px 24px 2px rgba(0,0,0,.14),0 6px 30px 5px rgba(0,0,0,.12),0 8px 10px -5px rgba(0,0,0,.2);
+  overflow: hidden;
 }
 .error{
   z-index: 10;
--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="dashboard">
-        <h1>Kezdőlap</h1>
-        <h2>Üdv a CarPool alkalmazásban!</h2>
+    <div class="dashboardBox">
+      <h1 id="title">Üdv a CarPool appban!</h1>
+      <h4>Elintézni valód akad a városban?</h4>
+      <h4 class="right">Szükséged van egy autóra?</h4>
+      <h4>Foglald le az autót, menj el az úticélodra és parkolj le. Ennyi.</h4>
+    </div>
         <template v-if="!isLoading">
           <EventCard v-for="event in events" :key="event.id" :event="event" />
         </template>
@@ -36,5 +40,23 @@ export default {
 </script>
 
 <style scoped>
-
+#dashboard{
+  background-image: url("../assets/vwgolf_gauss.jpg");
+  background-size: contain;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-position: top;
+}
+.dashboardBox{
+  /*background-image: linear-gradient(#28a745, white);*/
+  padding: 10px;
+  color: white;
+  text-shadow: 2px 2px 3px black;
+}
+#title{
+  text-align: center;
+}
+.right{
+  text-align: right;
+}
 </style>
